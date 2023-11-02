d493e803e7e976a815ad70da1530e63c81e0f187
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-11-02 21:50:12 +0100

    Update
    - Dodanie nowej sekcji - "Aktualności"
    - Stworzenie kart z aktualnościami
    - Ostylowanie kart przy pomocy TailwindCSS
    - Stworzenie skryptu dodawania automatycznego kart do sekcji aktualności

---
+++ js/newsCards.js
@@ -0,0 +1,28 @@
+function xData() {
+    const posts = [
+        {
+            image: "../images/img/news/wystawa.jpg",
+            title: "I wystawa miłośników motoryzacji organizowana przez R/W Concept.",
+            description: "Pierwsza w słupcy tego rodzaju wystawa motoryzacji na targowisku miejskim w słupcy!",
+        },
+        {
+            image: "../images/img/news/mikolajki.jpg",
+            title: "Motoryzacyjne mikołajki w słupcy zorganizowane przez R/W Concept.",
+            description: "Motoryzacyjne Mikołajki w Słupcy, zorganizowane przez R/W Concept, to niepowtarzalne wydarzenie, które zgromadziło miłośników motoryzacji w jednym miejscu, oferując emocjonujący dzień pełen unikalnych prezentacji pojazdów i atrakcji związanych z motoryzacją.",
+        },
+        {
+            image: "../images/img/news/kino.jpg",
+            title: "R/W Concept - Otwarcie kina samochodowego.",
+            description: "Kino samochodowe zostało otwarte! Wszystkich miłośników kina i samochodów serdecznie zapraszamy!",
+        },
+        {
+            image: "../images/img/news/tor.jpg",
+            title: "Ruszył tor wyścigowy R/W Concept w Słupcy!",
+            description: "W tym tygodniu uruchomiony został tor wyścigowy do szkolenia i doskonalenia swoich umiejętności prowadzenia samochodu!",
+        }
+    ];
+
+    return {
+        posts: posts,
+    };
+}
