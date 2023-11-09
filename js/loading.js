fed12a006490ac176c7807c2e496e58d1775efdd
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-11-09 11:08:33 +0100

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,14 +1,14 @@
 // Loading page
 
-window.addEventListener('load', function() {
-    // Gdy strona zostanie w pełni załadowana
-    var loadingOverlay = document.getElementById('loading-overlay');
-    var pageContent = document.getElementById('page-content');
+// window.addEventListener('load', function() {
+//     // Gdy strona zostanie w pełni załadowana
+//     var loadingOverlay = document.getElementById('loading-overlay');
+//     var pageContent = document.getElementById('page-content');
 
-    loadingOverlay.style.display = 'none'; // Ukryj animację ładowania
-    pageContent.style.display = 'block'; // Wyświetl zawartość strony
-    document.body.classList.remove('loading'); // Odblokuj scrollowanie
-});
+//     loadingOverlay.style.display = 'none'; // Ukryj animację ładowania
+//     pageContent.style.display = 'block'; // Wyświetl zawartość strony
+//     document.body.classList.remove('loading'); // Odblokuj scrollowanie
+// });
 
 
 
