42869517141fd1bb0a4d5722755bbf1123d47bed
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-11-10 12:04:15 +0100

    Little bit edit

--- dist/js/newsCards.js
+++ dist/js/newsCards.js
@@ -2,23 +2,23 @@ function xData() {
     const posts = [
         {
             linkToNewsPage: "./pages/news.html#news1",
-            image: "./images/img/galery/4.11/1.jpg",
+            image: "./images/aktualnosci/wystawa.webp",
             title: "I wystawa miłośników motoryzacji organizowana przez R/W Concept.",
             description: "Zorganizowaliśmy pierwszą przez nasz zespół wystawę samochodów i motocykli na targowisku miejskim w Słupcy. Wszystko to w szczytnym celu...",
         },
         {
             linkToNewsPage: "./pages/news.html#news2",
-            image: "./images/img/news/mikolajki.jpg",
+            image: "./images/aktualnosci/mikolajki.jpg",
             title: "Motoryzacyjne mikołajki w słupcy zorganizowane przez R/W Concept.",
             description: "Motoryzacyjne Mikołajki w Słupcy, zorganizowane przez R/W Concept, to niepowtarzalne wydarzenie, które zgromadziło miłośników motoryzacji w jednym miejscu, oferując emocjonujący dzień pełen unikalnych prezentacji pojazdów i atrakcji związanych z motoryzacją.",
         },
         {
-            image: "./images/img/news/kino.jpg",
+            image: "./images/aktualnosci/kino.jpg",
             title: "R/W Concept - Otwarcie kina samochodowego.",
             description: "Kino samochodowe zostało otwarte! Wszystkich miłośników kina i samochodów serdecznie zapraszamy!",
         },
         {
-            image: "./images/img/news/tor.jpg",
+            image: "./images/aktualnosci/tor.jpg",
             title: "Ruszył tor wyścigowy R/W Concept w Słupcy!",
             description: "W tym tygodniu uruchomiony został tor wyścigowy do szkolenia i doskonalenia swoich umiejętności prowadzenia samochodu!",
         }
