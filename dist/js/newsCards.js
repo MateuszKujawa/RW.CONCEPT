1a69bfedf6061db177d062ec0674fb1e286a855a
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-11-10 09:48:22 +0100

    Update web folders

--- dist/js/newsCards.js
+++ dist/js/newsCards.js
@@ -1,24 +1,24 @@
 function xData() {
     const posts = [
         {
-            linkToNewsPage: "../dist/news.html#news1",
-            image: "../images/img/galery/4.11/1.jpg",
+            linkToNewsPage: "./pages/news.html#news1",
+            image: "./images/img/galery/4.11/1.jpg",
             title: "I wystawa miłośników motoryzacji organizowana przez R/W Concept.",
             description: "Zorganizowaliśmy pierwszą przez nasz zespół wystawę samochodów i motocykli na targowisku miejskim w Słupcy. Wszystko to w szczytnym celu...",
         },
         {
-            linkToNewsPage: "../dist/news.html#news2",
-            image: "../images/img/news/mikolajki.jpg",
+            linkToNewsPage: "./pages/news.html#news2",
+            image: "./images/img/news/mikolajki.jpg",
             title: "Motoryzacyjne mikołajki w słupcy zorganizowane przez R/W Concept.",
             description: "Motoryzacyjne Mikołajki w Słupcy, zorganizowane przez R/W Concept, to niepowtarzalne wydarzenie, które zgromadziło miłośników motoryzacji w jednym miejscu, oferując emocjonujący dzień pełen unikalnych prezentacji pojazdów i atrakcji związanych z motoryzacją.",
         },
         {
-            image: "../images/img/news/kino.jpg",
+            image: "./images/img/news/kino.jpg",
             title: "R/W Concept - Otwarcie kina samochodowego.",
             description: "Kino samochodowe zostało otwarte! Wszystkich miłośników kina i samochodów serdecznie zapraszamy!",
         },
         {
-            image: "../images/img/news/tor.jpg",
+            image: "./images/img/news/tor.jpg",
             title: "Ruszył tor wyścigowy R/W Concept w Słupcy!",
             description: "W tym tygodniu uruchomiony został tor wyścigowy do szkolenia i doskonalenia swoich umiejętności prowadzenia samochodu!",
         }
