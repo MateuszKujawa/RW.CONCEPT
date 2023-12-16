b6b8e0d5f68813f3b7315d2d0d09b4e5f90276a7
Mateusz Kujawa | Junior Front End Dev <124597227+MateuszKujawa@users.noreply.github.com>
2023-12-16 14:54:02 +0100

    dsds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/*.{html,js}"],
-  theme: {
+export const content = ["./dist/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      raleway: ['Raleway'],
+      roboto: ['Roboto'],
+      poppins: ['poppins']
+    },
+    colors: {
+      'darkgray': '#0F0F0F',
+    },
     extend: {
-      fontFamily: {
-        raleway: ['Raleway'],
-        roboto: ['Roboto'],
-        poppins: ['poppins']
-      },
-      colors: {
-        'darkgray': '#0F0F0F',
+      backgroundImage: {
+        'about-background-image': "url('/images/backgrounds/bg-about2.jpg')",
       },
     },
   },
+  variants: {},
   plugins: [],
-}
+};
